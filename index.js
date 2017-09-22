34250e033bb836a67be095ddcccdf8e54b24934f
goddy <onememeonly@gmail.com>
2017-09-22 06:15:11 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+const Discord = require("discord.js");
+const cbot = new Discord.Client();
+
+cbot.on('ready', () => {
+  console.log(`Logged in as ${cbot.user.tag}!`);
+  cbot.user.setPresence({ game: { name: `prefix is ;`, type: 0 }});
+});
+
+const prefix = ";"
+
+cbot.on('message', msg => {
+  if (msg.author.bot) return;
+  if (msg.guild.id !== "350441231937044491" && msg.content.startsWith(prefix + "ping" || "subscribe" || "unsubscribe") === true) {
+    return msg.channel.send("Sorry, my commands are not available on this server!")
+  }
+
+  if (msg.content.startsWith(prefix + 'ping')) {
+    msg.channel.send("Pinging...").then(sent => {
+    sent.edit(`Pong! (${sent.createdTimestamp - msg.createdTimestamp}ms)`)
+  })
+}
+
+  if (msg.content.startsWith(prefix + 'subscribe')) {
+    let args = msg.content.split(" ").slice(1).join(" ")
+    var announceRole = msg.guild.roles.find("name", "cat announcements")
+    if (!args) {
+      msg.channel.send("You're dumb. Provide args, please.")
+    } else if (args === "announcements") {
+      msg.channel.send("You've successfully subscribed to `announcements`!")
+      msg.member.addRole(announceRole)
+    }
+  }
+
+  if (msg.content.startsWith(prefix + 'unsubscribe')) {
+    let args = msg.content.split(" ").slice(1).join(" ")
+    var announceRole = msg.guild.roles.find("name", "cat announcements")
+    if (!args) {
+      msg.channel.send("You're dumb. Provide args, please.")
+    } else if (args === "announcements") {
+      msg.channel.send("You've successfully unsubscribed from `announcements`!")
+      msg.member.removeRole(announceRole)
+    }
+  }
+});
+
+cbot.login(process.env.BOT_TOKEN);
